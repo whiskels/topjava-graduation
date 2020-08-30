98c5b8ea213fe5136dfd0c533a4b9e23d9d6bd9e
whiskels <kuzmingeo@gmail.com>
2020-08-30 20:57:15 +0300

    Added basic vote service tests

--- src/test/java/com/whiskels/graduation/VoteTestData.java
+++ src/test/java/com/whiskels/graduation/VoteTestData.java
@@ -1,4 +1,26 @@
 package com.whiskels.graduation;
 
+import com.whiskels.graduation.model.Vote;
+
+import java.time.LocalDate;
+
+import static com.whiskels.graduation.RestaurantTestData.RESTAURANT_1;
+import static com.whiskels.graduation.RestaurantTestData.RESTAURANT_3;
+import static com.whiskels.graduation.UserTestData.USER;
+import static com.whiskels.graduation.model.AbstractBaseEntity.START_SEQ;
+
 public class VoteTestData {
+    public static TestMatcher<Vote> VOTE_MATCHER = TestMatcher.usingFieldsComparator();
+
+    public static final int VOTE_1_ID = START_SEQ + 6;
+    public static final int VOTE_2_ID = START_SEQ + 7;
+    public static final int VOTE_3_ID = START_SEQ + 8;
+
+    public static final Vote VOTE_1 = new Vote(VOTE_1_ID, LocalDate.of(2020, 8, 10), USER, RESTAURANT_1);
+    public static final Vote VOTE_2 = new Vote(VOTE_2_ID, LocalDate.of(2020, 8, 11), USER, RESTAURANT_1);
+    public static final Vote VOTE_3 = new Vote(VOTE_3_ID, LocalDate.of(2020, 8, 12), USER, RESTAURANT_3);
+
+    public static Vote getNewVote() {
+        return new Vote(null, LocalDate.now(), USER, RESTAURANT_1);
+    }
 }
---
+++ src/test/java/com/whiskels/graduation/service/VoteServiceTest.java
@@ -0,0 +1,53 @@
+package com.whiskels.graduation.service;
+
+import com.whiskels.graduation.model.Vote;
+import com.whiskels.graduation.util.exception.NotFoundException;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static com.whiskels.graduation.DishTestData.DISH_1_ID;
+import static com.whiskels.graduation.UserTestData.*;
+import static com.whiskels.graduation.VoteTestData.*;
+import static org.junit.Assert.assertThrows;
+
+public class VoteServiceTest extends AbstractServiceTest {
+    @Autowired
+    protected VoteService voteService;
+
+    @Test
+    @Transactional
+    public void vote() throws Exception {
+        Vote newVote = getNewVote();
+        Vote created = voteService.vote(newVote.getUser().id(), newVote.getRestaurant().id(), newVote.getDate().atTime(10, 0));
+        newVote.setId(created.getId());
+        VOTE_MATCHER.assertMatch(created,newVote);
+        VOTE_MATCHER.assertMatch(voteService.getByUserIdAAndDate(created.getUser().getId(), created.getDate()), newVote);
+    }
+
+    @Test
+    @Transactional
+    public void getAll() throws Exception {
+        List<Vote> voteList = voteService.getAll(USER_ID);
+        VOTE_MATCHER.assertMatch(voteList, VOTE_3, VOTE_2, VOTE_1);
+    }
+
+    @Test
+    public void delete() throws Exception {
+        voteService.delete(VOTE_1_ID, USER_ID);
+        assertThrows(NotFoundException.class, () -> voteService.getByUserIdAAndDate(USER_ID, VOTE_1.getDate()));
+    }
+
+    @Test
+    public void deleteNotFound() throws Exception {
+        assertThrows(NotFoundException.class, () -> voteService.delete(NOT_FOUND, USER_ID));
+    }
+
+    @Test
+    public void deleteNotOwn() throws Exception {
+        assertThrows(NotFoundException.class, () -> voteService.delete(DISH_1_ID, ADMIN_ID));
+    }
+
+}
