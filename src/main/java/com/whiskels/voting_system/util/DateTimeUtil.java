ec9a99ebac60cda16ab3c251364b32ae2ea48985
whiskels <kuzmingeo@gmail.com>
2020-11-09 11:19:20 +0300

    fix: removed unused code

--- src/main/java/com/whiskels/voting_system/util/DateTimeUtil.java
+++ src/main/java/com/whiskels/voting_system/util/DateTimeUtil.java
@@ -5,25 +5,12 @@ import org.springframework.util.StringUtils;
 
 import java.time.*;
 import java.time.format.DateTimeFormatter;
-import java.time.temporal.ChronoUnit;
 
 public class DateTimeUtil {
     public static final String DATE_TIME_PATTERN = "yyyy-MM-dd HH:mm";
     public static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern(DATE_TIME_PATTERN);
 
-    // DB doesn't support LocalDate.MIN/MAX
-    private static final LocalDateTime MIN_DATE = LocalDateTime.of(1, 1, 1, 0, 0);
-    private static final LocalDateTime MAX_DATE = LocalDateTime.of(3000, 1, 1, 0, 0);
-
     private DateTimeUtil() {
-    }
-
-    public static LocalDateTime atStartOfDayOrMin(LocalDate localDate) {
-        return localDate != null ? localDate.atStartOfDay() : MIN_DATE;
-    }
-
-    public static LocalDateTime atStartOfNextDayOrMax(LocalDate localDate) {
-        return localDate != null ? localDate.plus(1, ChronoUnit.DAYS).atStartOfDay() : MAX_DATE;
     }
 
     public static String toString(LocalDateTime ldt) {
