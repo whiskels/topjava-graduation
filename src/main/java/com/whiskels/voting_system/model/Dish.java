3db308bb80a3314fdaeee85b812e9e6fd22c4f7c
whiskels <kuzmingeo@gmail.com>
2020-11-10 11:34:00 +0300

    remove: unused constructor

--- src/main/java/com/whiskels/voting_system/model/Dish.java
+++ src/main/java/com/whiskels/voting_system/model/Dish.java
@@ -37,10 +37,6 @@ public class Dish extends AbstractNamedEntity implements HasId {
     @JsonBackReference(value = "restaurantDishes")
     private Restaurant restaurant;
 
-    public Dish(LocalDate localDate, String name, Long price, Restaurant restaurant) {
-        this(null, localDate, name, price, restaurant);
-    }
-
     public Dish(Integer id, LocalDate localDate, String name, Long price, Restaurant restaurant) {
         super(id, name);
         this.localDate = localDate;
