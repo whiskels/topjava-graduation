0adb72886b78a6f97e2ee9eec3b65b3ce694bbd8
whiskels <kuzmingeo@gmail.com>
2020-11-10 11:52:05 +0300

    update: additional logging

--- src/main/java/com/whiskels/voting_system/service/VoteService.java
+++ src/main/java/com/whiskels/voting_system/service/VoteService.java
@@ -10,6 +10,8 @@ import com.whiskels.voting_system.util.exception.NotFoundException;
 import com.whiskels.voting_system.util.exception.VoteDeadlineException;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -26,6 +28,8 @@ import static com.whiskels.voting_system.util.ValidationUtil.checkNotFoundWithId
 @Service
 @RequiredArgsConstructor
 public class VoteService {
+    protected final Logger log = LoggerFactory.getLogger(getClass());
+
     private final VoteRepository voteRepository;
     private final UserRepository userRepository;
     private final RestaurantRepository restaurantRepository;
@@ -43,14 +47,18 @@ public class VoteService {
         try {
             vote = getByUserIdAndDate(user, votingLocalDateTime.toLocalDate());
         } catch (NotFoundException e) {
+            log.debug("new vote from user {} for {}", userId, restaurantId);
             return voteRepository.save(new Vote(null, votingLocalDateTime.toLocalDate(), user, restaurant));
         }
 
         if (votingLocalDateTime.toLocalTime().isBefore(VOTE_DEADLINE)) {
             if (vote.getRestaurant().id() != restaurantId) {
                 vote.setRestaurant(restaurant);
+                log.debug("vote from user {} for restaurant {} was changed", userId, restaurantId);
                 return voteRepository.save(vote);
             }
+
+            log.debug("vote from user {} for restaurant {} not changed", userId, restaurantId);
             return vote;
         } else {
             throw new VoteDeadlineException("Vote deadline has already passed");
